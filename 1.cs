3cbfddf9542f09b090b8f61553a3f3399624b501
kibergus228 <Matviumotok@gmail.com>
2026-06-21 21:13:23 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введи своє ім'я: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Привіт, " + name + "!");
+    }
+}
